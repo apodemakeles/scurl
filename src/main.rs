99f32f7ef979af3220c0c44434b9dbb0b6498e97
caozheng <caozheng@trolia.com>
2022-06-03 21:02:51 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap:: { Subcommand, Parser, Args};
+
+/// a super curl
+#[derive(Parser, Debug)]
+#[clap()]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Subcommand, Debug)]
+#[clap()]
+enum SubCommand {
+    Get(GetCmd),
+}
+
+#[derive(Parser, Debug)]
+struct GetCmd{
+    url: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    println!("{:?}", opts)
 }
