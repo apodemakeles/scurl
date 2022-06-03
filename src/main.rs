71bd073b72fb7792641dbbf5b955dca5cd2ef3cf
caozheng <caozheng@trolia.com>
2022-06-03 23:10:47 +0800

    print status, header and pretty json body

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::str::FromStr;
 use std::string::ParseError;
 use clap:: {Subcommand, Parser};
-use reqwest::{Client, Url};
+use reqwest::{Client, header, Response, Url};
 use anyhow::Result;
+use colored::*;
+use mime::Mime;
 
 /// a super curl
 #[derive(Parser, Debug)]
@@ -10,6 +12,10 @@ use anyhow::Result;
 struct Opts {
     #[clap(subcommand)]
     subcmd: SubCommand,
+
+    /// print full message
+    #[clap(short)]
+    verbose: bool,
 }
 
 #[derive(Subcommand, Debug)]
@@ -33,7 +39,7 @@ fn parse_to_url(s: &str) -> Result<Url, String> {
 #[tokio::main]
 async fn main()-> Result<()> {
     let opts: Opts = Opts::parse();
-    println!("{:?}", opts);
+    // println!("{:?}", opts);
 
     let result = match opts.subcmd {
         SubCommand::Get(ref cmd) => execute_get(cmd, &opts).await?,
@@ -45,7 +51,44 @@ async fn main()-> Result<()> {
 async fn execute_get(cmd: &GetCmd, opts: &Opts) -> Result<()>{
     let client = Client::new();
     let resp = client.get(&cmd.url.to_string()).send().await?;
-    println!("{:?}", resp.text().await?);
+    Ok(print_resp(resp, opts).await?)
+}
+
+fn print_status(resp: &Response){
+    let status_line = format!("{:?}, {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status_line);
+}
+
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+
+    print!("\n");
+}
+
+fn get_content_type(resp: &Response) -> Option<Mime>{
+    resp.headers().get(header::CONTENT_TYPE).
+        map(|v|v.to_str().unwrap().parse().unwrap())
+}
+
+fn print_body(m: Option<Mime>, body: &String) {
+    match m{
+        Some(v) => if v == mime::APPLICATION_JSON{
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+        },
+        _ => println!("{}", body),
+    }
+}
+
+async fn print_resp(resp: Response, opts: &Opts)-> Result<()>{
+    if opts.verbose{
+        print_status(&resp);
+        print_headers(&resp);
+    }
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
 
     Ok(())
 }
